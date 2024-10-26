65d54766283b547059e57f176bd9f4f55f0f8000
Tyson Arellano <tyson.arellano@yahoo.com>
2024-10-25 19:38:48 -0600

    user story 1

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -1,5 +1,16 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		sc.close();
+	}
 
 }
